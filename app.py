452b9780fdd89f99c59de629a6b90d3eddf5ea07
SUNAINA <sunainasaini036@gmail.com>
2026-06-29 13:37:33 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+
+from flask import Flask,render_template
+app=Flask(__name__) #__main__
+print(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if(__name__=="__main__"):
+  app.run(host="0.0.0.0",port=5000,debug=True)
+  print("hello")
+
+
+
+  
